ea0a2eda7d7b1951a75b1d1ce205f4c747a02cb2
palomitaproy <palomita.c1@gmail.com>
2017-12-07 15:02:21 +0100

    initial commit express pack bootstrap gulp

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+const gulp = require('gulp')
+const browserSync = require('browserSync').create()
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const nodemon = require('gulp-autoprefixer')
+
+const PORT = 3000
+
+gulp.task('serve', ['nodemon', 'sass'], () => {
+
+    browserSync.init({
+      proxy: {
+        target: "localhost:3000",
+        proxyReq: [
+          function(proxyReq) {
+            proxyReq.setHeader('X-Forwarded-Host', 'localhost:8000');
+          }
+        ],
+      },
+      port: 8000,
+      open: false,
+      ui: {
+        port: 8001
+      },
+
+    });
+
+  gulp.watch(`./views/**/**/*.pug`).on('change', browserSync.reload);
+  gulp.watch(`./public/**/**/*.sass`, ['sass']);
+});
+
+gulp.task('sass', () => {
+  return gulp.src(`./public/stylesheets/style.sass`)
+    .pipe(sass({
+      errLogToConsole: true,
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(`./public/stylesheets/`))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('nodemon', () => {
+  nodemon({
+    script: './bin/www',
+    ext: 'js html',
+    env: { 'NODE_ENV': 'development' }
+  })
+})
+
+gulp.task('default', ['serve']);
